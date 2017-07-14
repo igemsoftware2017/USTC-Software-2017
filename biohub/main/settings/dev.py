91570d6781e8f5fc31c3d6427ffedab32031f48e
hsfzxjy <hsfzxjy@163.com>
2017-07-14 20:30:15 +0800

    Change timezone.

--- biohub/main/settings/dev.py
+++ biohub/main/settings/dev.py
@@ -2,6 +2,9 @@ from .base import *  # noqa: F403,F401
 import coloredlogs
 
 import os
+
+TIME_ZONE = 'Asia/Shanghai'
+
 os.environ.setdefault(
     'COLOREDLOGS_LOG_FORMAT',
     '%(name)s:%(funcName)s:%(lineno)d %(message)s'
