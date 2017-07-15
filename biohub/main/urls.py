d7d946a4dcc2e9ef6eacb5d371820697574a88cb
hsfzxjy <hsfzxjy@163.com>
2017-07-16 00:31:09 +0800

    Drop django admin.

--- biohub/main/urls.py
+++ biohub/main/urls.py
@@ -1,6 +1,8 @@
-from django.conf.urls import url
-from django.contrib import admin
+# from django.conf.urls import url
+# from django.contrib import admin
+
+from biohub.core.routes import urlpatterns as biohub_urlpatterns
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-]
+    # url(r'^admin/', admin.site.urls),
+] + biohub_urlpatterns
