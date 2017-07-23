3a3cc4e833b9c12151d50e9cccc1e4e61c0b03b4
songchaow <songchaow@outlook.com>
2017-07-23 17:32:29 +0800

    complete forum models due to forum design

--- biohub/forum/models/bio_models.py
+++ biohub/forum/models/bio_models.py
@@ -1,4 +1,24 @@
 from django.db import models
+from django.conf import settings
+
+MAX_LEN_FOR_ARTICLE = 5000
+
+
+class File(models.Model):
+    """
+    Files consists of photos and other files
+    """
+    filepointer = models.FileField(upload_to='upload')
+    description = models.TextField(max_length=100)
+
+
+class Article(models.Model):
+    """
+    Each article serves as one of the following: a lab record, a document or a commit object. 
+    The article has no 'name' field, for the name can be specified in text(using markdown)
+    """
+    text = models.TextField(max_length=MAX_LEN_FOR_ARTICLE)
+    files = models.ManyToManyField(File)
 
 
 class Part(models.Model):
@@ -8,3 +28,21 @@ class Part(models.Model):
     # a gene part has two strand, so use two fields to record the sequence.
     sequence_a = models.TextField()
     sequence_b = models.TextField()
+    document = models.OneToOneField(Article, on_delete=models.SET_NULL)
+    lab_record = models.OneToOneField(Article, on_delete=models.SET_NULL)
+
+
+class ModificationRequest(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    part = models.ForeignKey(Part, on_delete=models.SET_NULL)
+    message = models.TextField(max_length=100)
+    # whether the request is granted
+    granted = models.BooleanField(default=False)
+    commit_obj = models.OneToOneField(Article)
+
+
+class Device(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    parts = models.ManyToManyField(Part)
