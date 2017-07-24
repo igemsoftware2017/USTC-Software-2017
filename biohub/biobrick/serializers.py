bc2041e39d0b3933ef7880e65b2a0b043882c749
shirley-wu <shirley0@mail.ustc.edu.cn>
2017-07-24 20:59:50 +0800

    serializer

--- biohub/biobrick/serializers.py
+++ biohub/biobrick/serializers.py
@@ -1,3 +1,13 @@
-# from rest_framework import serializers
+from biohub.utils.rest.serializers import bind_model, ModelSerializer
 
 # Create your serializers here.
+from .models import Biobrick
+
+
+@bind_model(Biobrick)
+class BiobrickSerializer(ModelSerializer):
+    # A rough one...
+    # hasn't define the fields or the relationship between Biobrick and Feature
+    # hasn't define the urls ( which will be placed inside the class
+    class Meta:
+        model = Biobrick
