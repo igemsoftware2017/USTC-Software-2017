26e70e165fc1ce2fe156f1f26064c1c61b030e24
hsfzxjy <hsfzxjy@163.com>
2017-07-24 21:27:04 +0800

    Added API plugins list, tested.

--- biohub/core/plugins/urls.py
+++ biohub/core/plugins/urls.py
@@ -1,3 +1,7 @@
-from biohub.core.routes import register_api, register_default, url  # noqa
+from biohub.core.routes import register_api, url
 
-# Place your route definition here.
+from .views import plugins_list
+
+register_api(r'^plugins/', [
+    url(r'^$', plugins_list, name='list')
+], namespace='plugins')
