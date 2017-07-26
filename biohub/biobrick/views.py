97b116b793f8d6232ac3b9d06819102984b37a55
shirley-wu <shirley0@mail.ustc.edu.cn>
2017-07-27 00:19:13 +0800

    search

--- biohub/biobrick/views.py
+++ biohub/biobrick/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from rest_framework import viewsets
 
 from .models import Biobrick
@@ -7,6 +8,36 @@ from .serializers import BiobrickSerializer
 
 
 class BiobrickViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Biobrick.objects.all().order_by('part_name')
-    # will be replaced by a get queryset function to search
+    # The viewset for bbk, to list, search and retrieve.
+    queryset = Biobrick.objects.all()
     serializer_class = BiobrickSerializer
+
+    def filter_queryset(self, queryset):
+        # Return a queryset according to the request params
+        querydict = self.request.query_params
+
+        name = querydict.get('name')
+        if name:
+            queryset = queryset.filter(part_name__icontains=name)
+
+        sequence = querydict.get('sequence')
+        if sequence:
+            queryset = queryset.filter(sequence__icontains=sequence)
+
+        desc = querydict.get('desc', '')
+        and_list = querydict.get('and', '').split(' ')
+        desc += ''.join(' +' + s for s in and_list if s is not '')
+        or_list = querydict.get('or', '').split(' ')
+        desc += ''.join(' ' + s for s in or_list if s is not '')
+        not_list = querydict.get('not', '').split(' ')
+        desc += ''.join(' -' + s for s in not_list if s is not '')
+        if desc is not '':
+            queryset = queryset.filter(
+                Q(short_desc__search=desc) | Q(description__search=desc))
+
+        order = querydict.get('order')
+        order_choices = ['part_name', '-uses']
+        order = order if order in order_choices else 'part_name'
+        queryset = queryset.order_by(order)
+
+        return queryset
--- tests/biobrick/test_list.py
+++ tests/biobrick/test_list.py
@@ -2,10 +2,69 @@ from django.core.urlresolvers import reverse
 
 from rest_framework.test import APITestCase
 
+from biohub.biobrick.models import Biobrick
 
-class Test(APITestCase):
+
+class TestList(APITestCase):
+    # To test the list rest-api
 
     def test_empty(self):
         # Test the empty list with no data in the database
         resp = self.client.get(reverse('api:biobrick:biobrick-list'))
-        self.assertDictContainsSubset({'results': []}, resp.data)
+        self.assertEqual(len(resp.data['results']), 0)
+
+    def test_show(self):
+        # Test if biobrick-list can show the bbk
+        bbk = Biobrick.objects.create(part_name='asdfasvasvav')
+        resp = self.client.get(reverse('api:biobrick:biobrick-list'))
+        self.assertEqual(len(resp.data['results']), 1)
+        self.assertDictContainsSubset({
+            'part_name': bbk.part_name,
+        }, resp.data['results'][0])
+
+    def test_order(self):
+        # Test if biobrick-list can show the bbk in the specific order
+        bbk1 = Biobrick.objects.create(part_name='aaaaaaaa', uses=10)
+        bbk2 = Biobrick.objects.create(part_name='bbbbbbbb', uses=20)
+
+        resp = self.client.get(reverse('api:biobrick:biobrick-list'))
+        self.assertDictContainsSubset({
+            'part_name': bbk1.part_name,
+        }, resp.data['results'][0])
+
+        resp = self.client.get(reverse('api:biobrick:biobrick-list'),
+                               {'order': '-uses'})
+        self.assertDictContainsSubset({
+            'part_name': bbk2.part_name,
+        }, resp.data['results'][0])
+
+        resp = self.client.get(reverse('api:biobrick:biobrick-list'),
+                               {'order': '2333?'})
+        self.assertDictContainsSubset({
+            'part_name': bbk1.part_name,
+        }, resp.data['results'][0])
+
+
+class TestSearch(APITestCase):
+    # To test the search function. Unfortunately I cannot test search
+    # description, as the indexes have to be built directly in mysql
+
+    def setUp(self):
+        Biobrick.objects.create(part_name='name', sequence='sequence')
+        Biobrick.objects.create(part_name='specialname')
+
+    def test_name(self):
+        resp = self.client.get(reverse('api:biobrick:biobrick-list'),
+                               {'name': 'special'})
+        self.assertEqual(len(resp.data['results']), 1)
+        self.assertDictContainsSubset({
+            'part_name': 'specialname',
+        }, resp.data['results'][0])
+
+    def test_sequence(self):
+        resp = self.client.get(reverse('api:biobrick:biobrick-list'),
+                               {'sequence': 'quence'})
+        self.assertEqual(len(resp.data['results']), 1)
+        self.assertDictContainsSubset({
+            'part_name': 'name',
+        }, resp.data['results'][0])
