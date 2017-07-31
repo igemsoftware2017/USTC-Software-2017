8624ab2a9287c42ce8b98287e9e8dd931e946afb
songchaow <songchaow@outlook.com>
2017-07-31 21:36:13 +0800

    add serializers for ExperienceEntry and SeqFeature

--- biohub/forum/serializers/brick_serializers.py
+++ biohub/forum/serializers/brick_serializers.py
@@ -1,10 +1,29 @@
 from rest_framework import serializers
 from biohub.utils.rest.serializers import bind_model,\
     ModelSerializer
-from ..models import Brick
+from ..models import Brick, Experience, SeqFeature
+
+class SeqFeatureSerializer(ModelSerializer):
+
+    class Meta:
+        model = SeqFeature
+        exclude = ('brick')
+
+class ExperienceEntrySerializer(ModelSerializer):
+    """
+    Shows entrance information (title and id)
+    used to generate titles and URLs in frontend
+    """
+    class Meta:
+        model = Experience
+        fields = ('id','title')
+
+
 @bind_model(Brick)
 class BrickSerializer(ModelSerializer):
     
+    experience_set = ExperienceEntrySerializer(many=True,read_only=True)
+    seqFeatures = SeqFeatureSerializer(many=True,read_only=True)
     class Meta:
         model = Brick
         fields = '__all__'
