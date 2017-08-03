5c5dabd405d52baa7644d111d196b975e527c4eb
hsfzxjy <hsfzxjy@163.com>
2017-08-03 16:58:44 +0800

    Added followers.

--- biohub/accounts/models.py
+++ biohub/accounts/models.py
@@ -14,9 +14,30 @@ class User(AbstractUser):
     avatar_url = models.URLField(_('avatar'), default=DEFAULT_AVATAR_URL)
     education = models.CharField(_('education'), blank=True, max_length=200)
     major = models.TextField(_('major'), blank=True, max_length=200)
+    followers = models.ManyToManyField(
+        'self',
+        symmetrical=False,
+        related_name='following')
 
     @cached_property
     def api_url(self):
         from django.urls import reverse
 
         return reverse('api:accounts:user-detail', kwargs={'pk': self.id})
+
+    def follow(self, target_user):
+        """
+        To follow a specific user.
+        """
+
+        if (target_user.id == self.id or
+                target_user.followers.filter(pk=self.id).exists()):
+            return
+
+        target_user.followers.add(self)
+
+    def unfollow(self, target_user):
+        """
+        To unfollow a specific user.
+        """
+        target_user.followers.remove(self)
