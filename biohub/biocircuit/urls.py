f3848ffe240dc26061095601fb45bba06cf06f66
shirley-wu <shirley0@mail.ustc.edu.cn>
2017-08-11 20:43:51 +0800

    api

--- biohub/biocircuit/urls.py
+++ biohub/biocircuit/urls.py
@@ -7,7 +7,7 @@ from biohub.core.routes import register_api
 from . import views
 
 register_api(r'^biocircuit/', [
-    url(r'^build/$', views.BiocircuitView.as_view(), name='biocircuit-build'),
+    url(r'^biocircuit/(?P<expr>.+)/$', views.BiocircuitView.as_view(), name='biocircuit-build'),  # the name seems wierd.
     url(r'^gates/$', views.GatesView.as_view(), name='biocircuit-gates'),
     url(r'^score/$', views.ScoreView.as_view(), name='biocircuit-score'),
 ], 'biocircuit')
