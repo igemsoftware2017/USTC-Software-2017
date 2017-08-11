a8be5a106665a157f9c63ff6d35705b6168a5a4d
songchaow <songchaow@outlook.com>
2017-08-11 22:25:51 +0800

    add activity model and migrate

--- biohub/forum/models/activity_model.py
+++ biohub/forum/models/activity_model.py
@@ -1,17 +1,20 @@
 from django.conf import settings
 from django.db import models
 
+
 class Activity(models.Model):
     TYPE_CHOICES = (
         ('Experience', 'Experience'),
         ('Comment', 'Comment'),
         ('Star', 'Star'),
         ('Rating', 'Rating'),
-        ('Watch','Watch')
+        ('Watch', 'Watch')
     )
-    type_ = models.CharField(default='',max_length=15)
-    user = models.ForeignKeyField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name='activities')
+    type = models.CharField(default='', max_length=15)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE, related_name='activities')
     # expLink = models.
-    score = models.Float
-    partName
-    intro
+    score = models.DecimalField(
+        max_digits=2, decimal_places=1, default=0)  # eg: 3.7
+    partName = models.CharField(default='', max_length=15)
+    intro = models.CharField(default='', blank=True, max_length=50)
