82a53c47038954415d39e9c08bd55742e21081b2
songchaow <songchaow@outlook.com>
2017-08-12 16:33:08 +0800

    add activity serializers and some small codes

--- biohub/forum/models/__init__.py
+++ biohub/forum/models/__init__.py
@@ -1,3 +1,3 @@
 from .bio_models import Article, Brick, Experience, SeqFeature
 from .forum_models import Post
-from .activity_model import Activity
+from .activity_model import Activity, ActivityParam
