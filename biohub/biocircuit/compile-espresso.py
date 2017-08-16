f7bea4241bf1e1633dd6ef4bee6991a04e879871
shirley-wu <shirley0@mail.ustc.edu.cn>
2017-08-16 20:41:45 +0800

    travis

--- biohub/biocircuit/compile-espresso.py
+++ biohub/biocircuit/compile-espresso.py
@@ -3,15 +3,17 @@ import os
 import platform
 
 sysstr = sys.platform
+dirname = os.path.dirname(os.path.abspath(__file__))
 a, b, *c = platform.python_version().split('.')
+
 if sysstr == "linux" or sysstr == "linux2" or sysstr == "darwin":
     os.system(
-        "gcc -w ./espresso/*.c -fPIC -shared -o espresso.so -lpython" + a + "." + b + "m -I/usr/include/python" + a + "." + b + "m/")
+        "gcc -w " + dirname + "/espresso/*.c -fPIC -shared -o espresso.so -lpython" + a + "." + b + "m -I/usr/include/python" + a + "." + b + "m/")
 elif sysstr == "win32":
     os.system(
-        "gcc -w ./espresso/*.c -mdll -D MS_WIN32 -o espresso.pyd -lpython" + a + b + " -IC:\Python" + a + b + "\include -LC:\Python" + a + b + "\libs")
+        "gcc -w " + dirname + "/espresso/*.c -mdll -D MS_WIN32 -o espresso.pyd -lpython" + a + b + " -IC:\Python" + a + b + "\include -LC:\Python" + a + b + "\libs")
 elif sysstr == "win64":
     os.system(
-        "gcc -w ./espresso/*.c -mdll -D MS_WIN64 -o espresso.pyd -lpython" + a + b + " -IC:\Python" + a + b + "\include -LC:\Python" + a + b + "\libs")
+        "gcc -w " + dirname + "/espresso/*.c -mdll -D MS_WIN64 -o espresso.pyd -lpython" + a + b + " -IC:\Python" + a + b + "\include -LC:\Python" + a + b + "\libs")
 else:
     print('Unsupported OS!')
