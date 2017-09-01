fd0debcac57da41e01516db016d73cd160191d8d
shirley-wu <shirley0@mail.ustc.edu.cn>
2017-09-01 16:39:17 +0800

    test

--- tests/biobrick/test_search.py
+++ tests/biobrick/test_search.py
@@ -9,34 +9,27 @@ class TestSearch(APITestCase):
     # And don't worry if it does not show the specific answer 'cause
     # the indexes may be different.
 
-    def test_part_name(self):
-        resp = self.client.get(reverse('api:biobrick:biobrick-search'),
-                               {'part_name': '034'})
-        self.assertGreaterEqual(len(resp.data['results']), 1)
-        self.assertTrue('034' in resp.data['results'][0]['part_name'])
-
-    def test_sequence(self):
-        resp = self.client.get(reverse('api:biobrick:biobrick-search'),
-                               {'sequence': 'atac'})
-        self.assertGreaterEqual(len(resp.data['results']), 1)
-        self.assertTrue('atac' in resp.data['results'][0]['sequence'])
-
     def test_suggestion(self):
         resp = self.client.get(reverse('api:biobrick:biobrick-search'),
                                {'q': 'promotre'})
         self.assertEqual(len(resp.data['results']), 0)
         self.assertDictContainsSubset({'suggestion': 'promoter'}, resp.data)
 
-    def test_q(self):
+    def test_q_desc(self):
         resp = self.client.get(reverse('api:biobrick:biobrick-search'),
                                {'q': 'promoter'})
         self.assertGreaterEqual(len(resp.data['results']), 1)
-        bbk_data = resp.data['results'][0]
-        print('\nshort_desc: %s\ndescription: %s\nkey word: %s\n' % (bbk_data['short_desc'], bbk_data['description'], 'promoter'))
+        self.assertIn('promoter', resp.data['results'][0]['short_desc'])
+
+    def test_q_name(self):
+        resp = self.client.get(reverse('api:biobrick:biobrick-search'),
+                               {'q': 'J63006'})
+        self.assertGreaterEqual(len(resp.data['results']), 1)
+        self.assertEqual(resp.data['results'][0]['part_name'], 'BBa_J63006')
 
     def test_highlight(self):
         resp = self.client.get(reverse('api:biobrick:biobrick-search'),
                                {'q': 'promoter', 'highlight': None})
         self.assertGreaterEqual(len(resp.data['results']), 1)
-        bbk_data = resp.data['results'][0]
-        print('\nhighlighted: %s\nkey word: %s\n' % (bbk_data['highlighted'], 'promoter'))
+        self.assertIn('<class="highlight">promoter</class>',
+                      resp.data['results'][0]['highlighted'][0])
