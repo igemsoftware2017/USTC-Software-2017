aa914b737ffccafaea44f5fe67e7e0558c6a9061
hsfzxjy <hsfzxjy@163.com>
2017-09-08 22:14:27 +0800

    Optimized user list.

--- biohub/forum/serializers/brick_serializers.py
+++ biohub/forum/serializers/brick_serializers.py
@@ -13,10 +13,6 @@ class BrickSerializer(ModelSerializer):
     api_url = serializers.HyperlinkedIdentityField(
         view_name='api:forum:brick-detail')
     document = ArticleSerializer(read_only=True)
-    watch_users = UserSerializer(
-        fields=('id', 'username'), read_only=True, many=True)
-    rate_users = UserSerializer(
-        fields=('id', 'username'), read_only=True, many=True)
     experience_set = serializers.HyperlinkedRelatedField(
         read_only=True, many=True,
         view_name='api:forum:experience-detail'
@@ -26,9 +22,11 @@ class BrickSerializer(ModelSerializer):
     class Meta:
         model = Brick
         exclude = ('update_time',)
-        read_only_fields = ('id', 'name', 'designer', 'group_name', 'part_type',
-                            'nickname', 'part_status', 'sample_status', 'experience_status',
-                            'use_num', 'twin_num', 'document',
-                            'assembly_compatibility', 'parameters', 'categories',
-                            'sequence_a', 'sequence_b', 'sub_parts', 'watch_users',
-                            'rate_score', 'rate_num', 'rate_users', 'star_users', 'stars')
+        read_only_fields = (
+            'id', 'name', 'designer', 'group_name', 'part_type',
+            'nickname', 'part_status', 'sample_status', 'experience_status',
+            'use_num', 'twin_num', 'document',
+            'assembly_compatibility', 'parameters', 'categories',
+            'sequence_a', 'sequence_b', 'sub_parts',
+            'rate_score', 'rate_num', 'stars'
+        )
