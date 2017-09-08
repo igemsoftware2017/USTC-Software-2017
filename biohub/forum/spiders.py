e2a54bcf45da1903cf2970b0227187e83c738b58
hsfzxjy <hsfzxjy@163.com>
2017-09-08 19:53:57 +0800

    Optimized spiders.

--- biohub/forum/spiders.py
+++ biohub/forum/spiders.py
@@ -5,16 +5,24 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
-from biohub.forum.models import Brick, Article
+from biohub.forum.models import Brick, Article, SeqFeature
 from biohub.utils.htm2text import html2text
 
 GENE_MAP = {'a': 't', 't': 'a', 'c': 'g', 'g': 'c'}
 
 
 class BrickSpider:
+
     base_site = 'http://parts.igem.org/'
     registry_base_site = 'http://parts.igem.org/cgi/xml/part.cgi?'
     logger = logging.getLogger(__name__)
+
+    basic_info_regexps = {
+        'designer': re.compile(r'Designed by:\s*(.*?)\s*&nbsp;'),
+        'group_name': re.compile(r'Group:\s*(.*?)\s*&nbsp;'),
+        'part_type': re.compile(r"<div style='.*?'\s*title='Part Type'>\s*(.*?)</div>"),
+        'nickname': re.compile(r"<div style='.*?'\s*type='Nickname'>\s*(.*?)</div>")
+    }
 
     @staticmethod
     def reverse_sequence(sequence_a):
@@ -28,70 +36,106 @@ class BrickSpider:
         # final URL: http://parts.igem.org/Part:BBa_K314110
         if brick is None:
             brick = Brick(name=brick_name)
+
         raw_response = requests.get(
-            BrickSpider.base_site + 'Part:BBa_' + brick_name)
+            BrickSpider.base_site + 'Part:BBa_' + brick_name
+        )
         if raw_response.status_code == 404:
             raise Exception('The part does not exist on iGEM\'s website')
+
         raw_html = raw_response.text
         soup = BeautifulSoup(raw_html, "lxml")
+
         # fill name
         brick.name = brick_name
+
         # fetch Designer
-        brick.designer = re.search('Designed by:\s*(.*?)\s*&nbsp', raw_html) and re.search(
-            'Designed by:\s*(.*?)\s*&nbsp', raw_html).group(1) or ''
-        brick.group_name = re.search('Group:\s*(.*?)\s*&nbsp', raw_html) and re.search(
-            'Group:\s*(.*?)\s*&nbsp', raw_html).group(1) or ''
-        brick.part_type = re.search('<div style=\'.*?\'\s*title=\'Part Type\'>\s*(.*?)</div>', raw_html) and re.search(
-            '<div style=\'.*?\'\s*title=\'Part Type\'>\s*(.*?)</div>', raw_html).group(1) or ''
-        brick.nickname = re.search('<div style=\'.*?\'\s*type=\'Nickname\'>\s*(.*?)</div>', raw_html) and re.search(
-            '<div style=\'.*?\'\s*type=\'Nickname\'>\s*(.*?)</div>', raw_html).group(1) or ''
-        # TODO: add another way of fetch sequence features
-        fetch_here = bool(
-            re.search('<script>\s*(var\s*sequence.*?)\s*</script>', raw_html))
-        if(fetch_here):
-            raw_bioinfo = re.search('<script>\s*(var\s*sequence.*?)\s*</script>', raw_html) and re.search(
-                '<script>\s*(var\s*sequence.*?)\s*</script>', raw_html).group(1) or ''
-            brick.sequence_a = re.search(
-                'String\(\'(.*?)\'\)', raw_bioinfo) and re.search('String\(\'(.*?)\'\)', raw_bioinfo).group(1) or ''
+        for field, exp in self.basic_info_regexps.items():
+            search_result = exp.search(raw_html)
+            setattr(
+                brick, field,
+                search_result.group(1) if search_result else ''
+            )
+
+        sequence_result = re.search(r'<script>\s*(var\s*sequence.*?)\s*</script>', raw_html)
+        if sequence_result is not None:
+            raw_bioinfo = sequence_result.group(1)
+
+            seq_a_result = re.search(r"String\('(.*?)'\)", raw_bioinfo)
+            brick.sequence_a = seq_a_result.group(1) if seq_a_result is not None else ''
             brick.sequence_b = BrickSpider.reverse_sequence(brick.sequence_a)
-        # add char field with the suitable validator eg: "23,435" done!
-            sub_parts_data = re.search('subParts.*?new Array\s*\((.+?)\);', raw_bioinfo) and re.search(
-                'subParts.*?new Array\s*\((.+?)\);', raw_bioinfo).group(1) or ''
-            if sub_parts_data:
-                sub_parts_list = re.findall(
-                    'Part\s*\(\d*,\s*\'(.*?)\'.*?\)', sub_parts_data)
-                brick.sub_parts = ','.join(sub_parts_list)
+
+            sub_parts_result = re.search(r"subParts.*?new Array\s*\((.+?)\);", raw_bioinfo)
+            sub_parts_data = sub_parts_result.group(1) if sub_parts_result is not None else ''
+            brick.sub_parts = ','.join(
+                re.findall(
+                    r"Part\s*\(\d*,\s*'(.*?)'.*?\)",
+                    sub_parts_data
+                )
+            )
+
+            result = re.search(r'seqFeatures.*?new Array\s*\((.+?)\)', raw_bioinfo)
+            if result is not None:
+                seqFeatureList = re.findall(
+                    r"\['(.*?)'\s*,\s*(\d*)\s*,\s*(\d*)\s*,\s*'(.*?)',(\d*)\s*",
+                    result.group(1)
+                )
+                seq_features = [
+                    SeqFeature(
+                        feature_type=each[0],
+                        start_loc=int(each[1]),
+                        end_loc=int(each[2]),
+                        name=each[3],
+                        reserve=bool(int(each[4] or '0')),
+                    )
+                    for each in seqFeatureList
+                ]
             else:
-                brick.sub_parts = ''  # for stand-alone bricks
+                seq_features = []
+        else:
+            raw_response = requests.get(
+                BrickSpider.registry_base_site + 'part=BBa_' + brick_name)
+            _soup = BeautifulSoup(raw_response.text, "lxml-xml")
+            feature_set = _soup.find_all('feature')
+            seq_features = [
+                SeqFeature(
+                    feature_type=feature.type.text,
+                    start_loc=int(feature.startpos.text),
+                    end_loc=int(feature.endpos.text),
+                    name=feature.title.text,
+                    reserve=feature.direction.text == 'reverse',
+                )
+                for feature in feature_set
+            ]
+            brick.sequence_a = _soup.find('seq_data').text
+            brick.sequence_b = BrickSpider.reverse_sequence(brick.sequence_a)
+            subparts = _soup.find('deep_subparts').find_all('subpart')
+            subpart_set = [subpart.name.text[4:] for subpart in subparts]
+            brick.sub_parts = ','.join(subpart_set)
+
         # fetch assembly compatibility
         div = soup.find(class_='compatibility_div')
-        assembly_compatibility = div and [('box_green' in item['class'])
-                                          for item in div.ul.find_all('li')] or ''
-        brick.assembly_compatibility = div and json.dumps(
-            assembly_compatibility) or ''
-        div = soup.find(id='part_status_wrapper')
+        if div is not None:
+            brick.assembly_compatibility = json.dumps([
+                'box_green' in item['class']
+                for item in div.ul.find_all('li')
+            ])
+        else:
+            brick.assembly_compatibility = ''
+
+        divs = soup.find(id='part_status_wrapper').find_all('div')
         # fetch release status
-        div2 = div.find_all('div')[0]
-        brick.part_status = div2.text
+        brick.part_status = divs[0].text
         # fetch sample status
-        div2 = div.find_all('div')[1]
-        brick.sample_status = div2.text
+        brick.sample_status = divs[1].text
         # fetch experience status
-        div2 = div.find_all('div')[2]
-        brick.experience_status = div2.text
+        brick.experience_status = divs[2].text
         # fetch use number
-        div2 = div.find_all('div')[3]
-        if re.search('(\d+)\s*', div2.text):
-            brick.use_num = int(re.search('(\d+)\s*', div2.text).group(1))
-        else:
-            brick.use_num = 0
+        result = re.search(r"(\d+)\s*", divs[3].text)
+        brick.use_num = int(result.group(1)) if result is not None else 0
         # fetch twin num(if exists)
-        div2 = div.find_all('div')[4]
-        if re.search('(\d+)\s*Twins.*?', div2.text):
-            brick.twin_num = int(
-                re.search('(\d+)\s*Twins.*?', div2.text).group(1))
-        else:
-            brick.twin_num = 0
+        result = re.search(r"(\d+)\s*Twins.*?", divs[4].text)
+        brick.twin_num = int(result.group(1)) if result is not None else 0
 
         # fetch parameters
         parameters = []
@@ -101,13 +145,15 @@ class BrickSpider:
         else:
             for entry in div.table.find_all('tr'):
                 parameters.append(
-                    [element.text for element in entry.find_all('td')])
+                    [element.text for element in entry.find_all('td')]
+                )
             brick.parameters = json.dumps(parameters)
+
         # fetch categories
         div = soup.find(id='categories')
-        categories = re.search(
-            '(//.*?)\s*\Z', div.text, re.DOTALL) and re.search('(//.*?)\s*\Z', div.text, re.DOTALL).group(1) or ''
-        brick.categories = categories
+        result = re.search(r'(//.*?)\s*\Z', div.text, re.DOTALL)
+        brick.categories = result.group(1) if result is not None else ''
+
         soup = soup.find('div', id='mw-content-text')
         # remove scripts, panel, and compatibility infos
         scriptset = soup.find_all(name='script')
@@ -122,8 +168,9 @@ class BrickSpider:
         compat = soup.find(class_='compatibility_div')
         if compat:
             compat.parent.extract()
+
         # restore images by supplementing URLs
-        newdoc = re.sub('=\"/(.*?\")', '=\"' +
+        newdoc = re.sub(r'="/(.*?")', '="' +
                         BrickSpider.base_site + r'\1', str(soup))
         soup2 = BeautifulSoup(newdoc, "lxml")
         h = html2text.HTML2Text()
@@ -132,32 +179,11 @@ class BrickSpider:
         article = Article.objects.create(text=markdown)  # attach no files
         brick.document = article
         brick.save()
-        # this must be executed after the brick has been saved
-        if(fetch_here):
-            if(re.search(
-                    'seqFeatures.*?new Array\s*\((.+?)\)', raw_bioinfo)):
-                seqFeature_data = re.search(
-                    'seqFeatures.*?new Array\s*\((.+?)\)', raw_bioinfo).group(1)
-                seqFeatureList = re.findall(
-                    '\[\'(.*?)\'\s*,\s*(\d*)\s*,\s*(\d*)\s*,\s*\'(.*?)\',(\d*)\s*', seqFeature_data)
-                for each in seqFeatureList:
-                    brick.seqFeatures.create(feature_type=each[0], start_loc=int(
-                        each[1]), end_loc=int(each[2]), name=each[3], reserve=bool(each[4]))
-        else:
-            # go to another page to fetch sequence features
-            raw_response = requests.get(
-                BrickSpider.registry_base_site + 'part=BBa_' + brick_name)
-            soup = BeautifulSoup(raw_response.text, "lxml-xml")
-            feature_set = soup.find_all('feature')
-            for feature in feature_set:
-                brick.seqFeatures.create(feature_type=feature.type.text, start_loc=int(feature.startpos.text), end_loc=int(
-                    feature.endpos.text), name=feature.title.text, reserve=(feature.direction.text == 'reserve'))
-            brick.sequence_a = soup.find('seq_data').text
-            brick.sequence_b = BrickSpider.reverse_sequence(brick.sequence_a)
-            subparts = soup.find('deep_subparts').find_all('subpart')
-            subpart_set = [subpart.name.text[4:] for subpart in subparts]
-            brick.sub_parts = ','.join(subpart_set)
-            brick.save()
+
+        for feature in seq_features:
+            feature.brick = brick
+
+        SeqFeature.objects.bulk_create(seq_features)
 
         return True
 
@@ -172,29 +198,41 @@ class ExperienceSpider:
     def fill_from_page(self, brick_name):
 
         brick = Brick.objects.get(name=brick_name)
+
         raw_response = requests.get(
             ExperienceSpider.base_site + 'Part:BBa_' + brick_name + ':Experience')
         if raw_response.status_code == 404:
             raise Exception('The experience does not exist on iGEM\'s website')
+
         raw_html = raw_response.text
         soup = BeautifulSoup(raw_html, "lxml")
         soup = soup.find('div', id='mw-content-text')
-        for rubbish in soup.find_all('p', text=re.compile('UNIQ')):
+
+        for rubbish in soup.find_all('p', text=re.compile('\x7fUNIQ')):
             rubbish.extract()
         # determine the structure of user reviews (there are 2 types)
         beginning = soup.find(id='User_Reviews').parent
-        if beginning.find_next_siblings('table'):
+        tables = beginning.find_next_siblings('table')
+        if tables:
             # the first type
-            for entry in beginning.find_next_siblings('table'):
-                author_name = entry.tr.find_all('td')[0].p.text
+            for entry in tables:
+                tds = entry.tr.find_all('td')
                 author_name = re.search(
-                    '\s*(.*?)\s*$', author_name, re.DOTALL).group(1)
-                experience = brick.experience_set.get_or_create(
-                    author_name=author_name, defaults={'title': '', 'brick': brick})[0]
-                content_html = entry.tr.find_all('td')[1]
+                    r'\s*(.*?)\s*$',
+                    tds[0].p.text,
+                    re.DOTALL
+                ).group(1)
+                experience, _ = brick.experience_set.get_or_create(
+                    author_name=author_name,
+                    defaults={'title': '', 'brick': brick}
+                )
+                content_html = tds[1]
                 # change images' URLs to absolute ones
-                restored_content = re.sub('=\"/(.*?\")', '=\"' +
-                                          ExperienceSpider.base_site + r'\1', str(content_html))
+                restored_content = re.sub(
+                    r'="/(.*?")',
+                    '="' + ExperienceSpider.base_site + r'\1',
+                    str(content_html)
+                )
                 h = html2text.HTML2Text()
                 h.body_width = 1000
                 markdown = h.handle(restored_content)
@@ -209,12 +247,16 @@ class ExperienceSpider:
             content = None
             experience = None
             for para in beginning.find_next_siblings('p'):
-                if re.match(r'\s*igem.{1,60}$', para.text, re.IGNORECASE):
+                matched = re.match(r'\s*igem.{1,60}$', para.text, re.IGNORECASE)
+                if matched:
                     # save previous collected content
                     # change images' URLs to absolute ones
                     if content and experience:
-                        restored_content = re.sub('=\"/(.*?\")', '=\"' +
-                                                  ExperienceSpider.base_site + r'\1', str(content))
+                        restored_content = re.sub(
+                            r'="/(.*?")',
+                            '="' + ExperienceSpider.base_site + r'\1',
+                            str(content)
+                        )
                         h = html2text.HTML2Text()
                         h.body_width = 1000
                         markdown = h.handle(restored_content)
@@ -228,10 +270,11 @@ class ExperienceSpider:
 
                     content = None
                     # create the next user review
-                    author_name = re.match(
-                        '\s*igem.{1,60}$', para.text, re.IGNORECASE).group(0)
-                    experience = brick.experience_set.get_or_create(
-                        author_name=author_name, defaults={'title': '', 'brick': brick})[0]
+                    author_name = matched.group(0)
+                    experience, _ = brick.experience_set.get_or_create(
+                        author_name=author_name,
+                        defaults={'title': '', 'brick': brick}
+                    )
                 else:
                     if experience:  # created last time in 'if' branch
                         # collect contents
