4152163e6da1d893625f55d6f8552d32b743d895
hsfzxjy <hsfzxjy@163.com>
2017-10-14 19:51:19 +0800

    Fixed path.

--- biohub/main/asgi.py
+++ biohub/main/asgi.py
@@ -1,9 +1,10 @@
 import os
 import sys
-import biohub.compat  # noqa
-from channels.asgi import get_channel_layer
-
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..'))
+
+import biohub.compat  # noqa
+from channels.asgi import get_channel_layer  # noqa
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "biohub.main.settings.prod")
 os.environ.setdefault(
