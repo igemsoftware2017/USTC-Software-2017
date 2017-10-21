d7771e293f2b654253cb379134b3f186da89964a
hsfzxjy <hsfzxjy@163.com>
2017-10-21 15:01:50 +0800

    [Added] js url.

--- biohub/biomap/apps.py
+++ biohub/biomap/apps.py
@@ -7,3 +7,4 @@ class BiomapConfig(PluginConfig):
     title = 'BioMap'
     author = 'hsfzxjy'
     description = 'A tool to analyze relationships between bricks.'
+    js_url = 'https://ustc-software2017-frontend.github.io/biohub-plugin-BioMap/build/plugin.js'
