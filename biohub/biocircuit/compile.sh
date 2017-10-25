df836f69a99a8aaccb1b7126eb6c650751561198
hsfzxjy <hsfzxjy@163.com>
2017-10-25 20:12:40 +0800

    Rollback biocircuit.

--- biohub/biocircuit/compile.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-rm *.pyc
-rm *.o
-rm *.so
-gcc -c simulate.c -std=c99 -fPIC -O0 -o simulate.o
-gcc simulate.o -shared -o simulate.so
--- biohub/biocircuit/views.py
+++ biohub/biocircuit/views.py
@@ -18,7 +18,6 @@ from . import biocircuit as biocircuit
 from .biogate_man import load_gates_json, biogate
 from . import bio_system as biosystem
 from . import bio_system_cache as cache
-from biohub.biobrick.models import Part as Parts
 
 
 class BiocircuitView(APIView):
@@ -95,75 +94,3 @@ class GatesView(APIView):
             response["status"] = "failed"
             response["detail"] = error.__doc__
             return Response(response, status=status.HTTP_400_BAD_REQUEST)
-
-
-class SimulateView(APIView):
-    """URI /simulate return the simulate result of a biological circuit
-
-    Usage:
-    """
-    parser_classes = (JSONParser,)
-    renderer_classes = (JSONRenderer,)
-
-    def post(self, request, format=None):
-        """Only accept application/json type.
-
-        """
-        try:
-            # request.data is the graph of the bio-system
-            '''
-            system_data = biosystem.bio_system(request.data)
-            cache_data = cache.biosystem_cache(request.data)
-            if cache_data:  # != None, better not compare with None.
-                response_from_back = cache_data
-            else:
-                system_data.simulation()
-                response_from_back = system_data.record_list
-                cache.biosystem_update_cache(request.data, response_from_back)
-            '''
-            cache_data = cache.biosystem_cache(request.data)
-            if cache_data:  # != None, better not compare with None.
-                response_from_back = cache_data
-            else:
-                response_from_back = biosystem.simulate(request.data)
-                cache.biosystem_update_cache(request.data, response_from_back)
-        except BaseException as error:
-            raise
-            response = {"status": "failed", "detail": error.message}
-            return Response(response, status=status.HTTP_400_BAD_REQUEST)
-        # assert isinstance(response_from_back, list)
-        return Response(response_from_back)
-
-
-class PartsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Parts
-
-
-class PartsView(APIView):
-    """This URI return frontend parts info in the database.
-
-    Usage:
-    """
-    parser_classes = (JSONParser,)
-    renderer_classes = (JSONRenderer,)
-
-    def post(self, request, format=None):
-        """
-        deal with the POST method only
-        """
-        response_total = []
-        for item in request.data:
-            pre_filter = {}
-            if "id" in item:
-                pre_filter['id'] = item['id']
-            for key in item:
-                if key != "id":
-                    pre_filter[key + "__contains"] = item[key]
-            queryset = Parts.objects.filter(**pre_filter)
-            serializer = PartsSerializer(queryset, many=True)
-            response_total.extend(serializer.data)
-        response_dict = {}
-        response_dict['status'] = "SUCCESS"
-        response_dict['data'] = response_total
-        return Response(response_dict)
