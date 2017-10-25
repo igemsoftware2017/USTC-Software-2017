9ac92e5c956023642ab8cb1bfb9e8f835e86a26e
hsfzxjy <hsfzxjy@163.com>
2017-10-25 18:22:21 +0800

    [Added] Description.

--- biohub/biomap/apps.py
+++ biohub/biomap/apps.py
@@ -6,5 +6,5 @@ class BiomapConfig(PluginConfig):
     name = 'biohub.biomap'
     title = 'BioMap'
     author = 'hsfzxjy'
-    description = 'A tool to analyze relationships between bricks.'
+    description = 'A tool to analyze relationships between bricks. Just input the full name of a specific part (e.g. BBa_B0015) and click the corresponding button, the plugin will show you the relationship between it and other parts.'
     js_url = 'https://ustc-software2017-frontend.github.io/biohub-plugin-BioMap/build/plugin.js'
