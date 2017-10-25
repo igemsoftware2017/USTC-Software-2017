d826cecb239ddece3f7e873f5afd2e0b59029148
hsfzxjy <hsfzxjy@163.com>
2017-10-25 18:18:13 +0800

    [Added] Description.

--- biohub/abacus/apps.py
+++ biohub/abacus/apps.py
@@ -5,6 +5,6 @@ class AbacusConfig(PluginConfig):
 
     name = 'biohub.abacus'
     title = 'ABACUS'
-    author = 'Jay Lan, Jiansi Li'
-    description = ''
+    author = 'Jay Lan, Jiansi Li, hsfzxjy'
+    description = 'ABACUS is a tool to design amino acid sequence from a given protein. Proteins are specified in Protein Data Bank format. The uploaded .pdb will be evaludated by simulated annealing algorithm, and another .pdb file representing the result will be returned.'
     js_url = 'https://ustc-software2017-frontend.github.io/biohub-plugin-ABACUS/build/plugin.js'
--- biohub/biocircuit/apps.py
+++ biohub/biocircuit/apps.py
@@ -6,5 +6,5 @@ class BiocircuitConfig(PluginConfig):
     name = 'biohub.biocircuit'
     title = 'Biocircuit'
     author = 'zml, wxq'
-    description = ''
+    description = 'The design of robust and sensitive biological circuit are always bothering. To rationally design synthetic gene circuits with specific functions, we write BioCircuit. The plugin is an adpatation of the project BioBLESS developed by USTC-Software 2015, with the ability to design digital circuit, biological circuit as well as analyze the performance of them.'
     js_url = 'https://ustc-software2017-frontend.github.io/biohub-plugin-bioBLESS/build/plugin.js'
